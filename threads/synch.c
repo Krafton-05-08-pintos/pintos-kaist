236b780049d9aee25d34265f0ba3867228d5f0db
AruJoy <wkrjsdl5632@gmail.com>
2024-05-16 08:31:53 +0000

    Feat: donate_one pass

--- threads/synch.c
+++ threads/synch.c
@@ -191,7 +191,11 @@ lock_acquire (struct lock *lock) {
 	ASSERT (lock != NULL);
 	ASSERT (!intr_context ());
 	ASSERT (!lock_held_by_current_thread (lock));
-
+	if(lock ->holder != NULL){
+		if(lock->holder->priority < thread_current() -> priority){
+			lock->holder->priority = thread_current() -> priority;
+		}
+	}
 	sema_down (&lock->semaphore);
 	lock->holder = thread_current ();
 }
