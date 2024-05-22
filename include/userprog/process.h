b23310676077c16163bcc9c24454b58a2154f715
AruJoy <wkrjsdl5632@gmail.com>
2024-05-22 13:12:01 +0000

    중간 저장

--- include/userprog/process.h
+++ include/userprog/process.h
@@ -9,5 +9,7 @@ int process_exec (void *f_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (struct thread *next);
+/* 작성함수 */
+char **str_split(char* str, const char *delimiters);
 
 #endif /* userprog/process.h */
